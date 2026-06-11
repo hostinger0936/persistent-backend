28fd41ee2aef1e542a515e474ca26ccb31c29d16
hostinger0936 <dk420420boss@gmail.com>
2026-06-11 01:22:55 -0700

    Update Device.ts

--- src/models/Device.ts
+++ src/models/Device.ts
@@ -1,4 +1,4 @@
-import mongoose, { Document, Schema } from "mongoose";
+import mongoose, { Document, Schema } from "mongoose"; 
 export interface SimInfo {
   uniqueid: string;
   sim1Number?: string;
